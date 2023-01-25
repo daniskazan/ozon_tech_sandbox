6b6bb7e5d6bf03c00772d9d5f9c558f39510dd4d
daniskazan <danis.sharafiev@bk.ru>
2023-01-25 16:01:23 +0300

    ! first 2 tasks

---
+++ sales.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+def main():
+    case_count = int(input())
+    for _ in range(case_count):
+        amount = int(input())
+        prices = [int(price) for price in input().split(maxsplit=amount)]
+        c = Counter(prices)
+        total = 0
+        for price, product_amount in c.items():
+            discount_part = ((product_amount // 3) * 2 * price)
+            rest_part = (product_amount % 3) * price
+            total += discount_part + rest_part
+        print(total)
+
+
+if __name__ == '__main__':
+    main()
---
+++ summator.py
@@ -0,0 +1,12 @@
+
+
+
+def main():
+    case_count = int(input())
+    for _ in range(case_count):
+        a, b = map(int, input().split())
+        print(a + b)
+
+
+if __name__ == '__main__':
+    main()
