f615d0ec41c91b5a266b126a105e08d019015e26
daniskazan <danis.sharafiev@bk.ru>
2023-02-01 12:56:35 +0300

    + task6 10/20 balls

--- g.py
+++ g.py
@@ -13,34 +13,42 @@ def get_friends_map() -> Dict[int, list]:
 
 def get_possible_friends(mapping: Dict):
     uids = list(mapping.keys())
-    print(uids)
-    possible_friends = {user_id: set() for user_id in uids}
+    # print(uids)
+    possible_friends = {user_id: {0} for user_id in uids}
     for uid in uids:
+        max_mutual_friends_amount = -1
         current_user_friends = mapping.get(uid)
         other_people = [people for people in uids if people not in current_user_friends and people != uid]
 
-        print(f"OTHER PEOPLE {other_people}")
-        print()
+        # print(f"OTHER PEOPLE {other_people}")
+        # print()
+
+        if max_mutual_friends_amount > 5:
+            continue
 
         for other in other_people:
             other_user_friends = mapping.get(other)
             mutual_friends = tuple((set(current_user_friends) & set(other_user_friends)))
-            print(f"USER_ID = {uid}, his friends is {current_user_friends}")
-            print(f"OTHER_USER_ID = {other}, his friends is {other_user_friends}")
-            print(f"THEIR MUTUAL FRIENDS ARE {mutual_friends}")
-            print("=============")
-            if mutual_friends:
+            # print(f"USER_ID = {uid}, his friends is {current_user_friends}")
+            # print(f"OTHER_USER_ID = {other}, his friends is {other_user_friends}")
+            # print(f"THEIR MUTUAL FRIENDS ARE {mutual_friends}")
+            # print("=============")
+            if all([mutual_friends, len(mutual_friends) > max_mutual_friends_amount]):
+                # удалить всех из списка возможных друзей
+                max_mutual_friends_amount = len(mutual_friends)
+                possible_friends[uid] = {other}
+            elif len(mutual_friends) == max_mutual_friends_amount:
                 possible_friends[uid].add(other)
-                possible_friends[other].add(uid)
 
     return possible_friends
 
 
 def main():
     friends_map = get_friends_map()
-    print(friends_map)
+    # print(friends_map)
     possible_friends = get_possible_friends(friends_map)
-    print(possible_friends)
+    for v in possible_friends.values():
+        print(*sorted(v))
 
 
 if __name__ == '__main__':
