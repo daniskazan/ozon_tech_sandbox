381a3794430aeb57a934362f7b66da1446f8e12a
daniskazan <danis.sharafiev@bk.ru>
2023-02-03 00:14:16 +0300

    + taskB(price after discount):fire:

--- sales.py
+++ sales.py
@@ -1,12 +1,26 @@
 from collections import Counter
+from collections import namedtuple
+from typing import List
+
+TestCase = namedtuple("TestCase", ["number_of_purchased_items", "prices"])
+
+
+def prepare_tests() -> List[TestCase]:
+    tests = []
+    cases_amount = int(input())
+    for _ in range(cases_amount):
+        number_of_purchased_items = int(input())
+        prices = [int(price) for price in input().split(maxsplit=number_of_purchased_items + 1)]
+        test_case = TestCase(number_of_purchased_items, prices)
+        tests.append(test_case)
+    return tests
 
 
 def main():
-    case_count = int(input())
-    for _ in range(case_count):
-        amount = int(input())
-        prices = [int(price) for price in input().split(maxsplit=amount)]
-        c = Counter(prices)
+    tests = prepare_tests()
+    for t in tests:
+        c = Counter(t.prices)  # считаем количество купленных товаров
+
         total = 0
         for price, product_amount in c.items():
             discount_part = (product_amount // 3) * 2 * price
